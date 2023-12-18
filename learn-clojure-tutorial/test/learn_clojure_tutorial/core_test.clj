89032bb5ec50ce331da79622f026603b4bdb6c0a
Andy Tebu <andytebu>
2023-12-18 10:57:09 -0300

    Add Clojure tutorial files and initial configuration

---
+++ learn-clojure-tutorial/test/learn_clojure_tutorial/core_test.clj
@@ -0,0 +1,7 @@
+(ns learn-clojure-tutorial.core-test
+  (:require [clojure.test :refer [deftest is testing]]
+            [learn-clojure-tutorial.core :as core]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 1 1))))
